5462e35e476e94f6f26508419b612b24513b90d4
Igor Zolov <i.zolov@yandex.ru>
2026-05-06 23:43:10 +0300

    Template preparation and creation scripts fixed

--- proxmox/templates/create-template.sh
+++ proxmox/templates/create-template.sh
@@ -2,16 +2,14 @@
 
 set -euo pipefail
 
-: "${CI_USER:=debian}"
-: "${PASSWD:=debian}"
+: "${CI_USER:=ubuntu}"
+: "${PASSWD:=ubuntu}"
 : "${VM_ID:=5000}"
-: "${VM_NAME:=debian-13-template}"
+: "${VM_NAME:=ubuntu-template}"
 : "${STORAGE:=local-lvm}"
-: "${IMG_DIR:=/var/lib/vz/template/iso}"
-: "${IMG_NAME:=debian-13-generic-amd64.qcow2}"
+: "${IMG_DIR:=/var/pve/vm/template}"
+: "${IMG_NAME:=ubuntu-template.qcow2}"
 : "${IMG_PATH:=${IMG_DIR}/${IMG_NAME}}"
-: "${SNIPPETS_DIR:=/var/lib/vz/snippets}"
-: "${CLOUDINIT_SNIPPET:=${SNIPPETS_DIR}/${VM_ID}-cloudinit.yaml}"
 : "${MEMORY:=2048}"
 : "${CORES:=2}"
 : "${DISK_SIZE:=10G}"
@@ -37,43 +35,31 @@ qm importdisk "${VM_ID}" "${IMG_PATH}" "${STORAGE}"
 echo "===> Attaching disk"
 qm set "${VM_ID}" \
   --scsihw virtio-scsi-pci \
-  --scsi0 "${STORAGE}:vm-${VM_ID}-disk-0"
+  --scsi0 "${STORAGE}:vm-${VM_ID}-disk-0,cache=writeback,discard=on,ssd=1"
 
 echo "===> Adding cloud-init"
-qm set "${VM_ID}" --boot order=scsi0
 qm set "${VM_ID}" --boot c --bootdisk scsi0
-qm set "${VM_ID}" --ide0 "${STORAGE}:cloudinit"
+qm set "${VM_ID}" --boot order=scsi0
+qm set "${VM_ID}" --ide1 "${STORAGE}":cloudinit
+
+qm set "${VM_ID}" --agent enabled=1
+
+echo "===> Resizing disk +${DISK_SIZE}"
+qm resize "${VM_ID}" scsi0 "+${DISK_SIZE}"
 
 qm set "${VM_ID}" --serial0 socket --vga serial0
 qm set "${VM_ID}" --cpu "cputype=${CPU_TYPE}"
-
-echo "===> Enabling QEMU Guest Agent"
-qm set "${VM_ID}" --agent enabled=1
-
-echo "===> Resizing disk to ${DISK_SIZE}"
-qm resize "${VM_ID}" scsi0 "${DISK_SIZE}"
+qm set "${VM_ID}" --ostype l26
 
 qm set "${VM_ID}" \
   --ciuser "${CI_USER}" \
   --cipassword "${PASSWD}" \
+  --ciupgrade 1 \
   --sshkeys ~/.ssh/homelab.pub \
-  --sshkeys ~/.ssh/id_rsa.pub \
-  --ipconfig0 ip=dhcp
+  --ipconfig0 ip=dhcp \
+  --nameserver 192.168.8.1 \
+  --searchdomain cloud.local
 
-echo "===> Creating cloud-init snippet"
-mkdir -p "${SNIPPETS_DIR}"
-
-cat <<EOF > "${CLOUDINIT_SNIPPET}"
-#cloud-config
-package_update: true
-packages:
-  - qemu-guest-agent
-runcmd:
-  - systemctl enable qemu-guest-agent
-  - systemctl start qemu-guest-agent
-EOF
-
-qm set "${VM_ID}" --cicustom "user=local:snippets/${VM_ID}-cloudinit.yaml"
 
 echo "===> Converting VM to template"
 qm template "${VM_ID}"
--- proxmox/templates/prepare-template-image.sh
+++ proxmox/templates/prepare-template-image.sh
@@ -2,30 +2,37 @@
 
 set -euo pipefail
 
-: "${PASSWD:=debian}"
-: "${IMG_DIR:=/var/lib/vz/template/iso}"
-: "${IMG_NAME:=debian-13-generic-amd64.qcow2}"
-: "${IMG_PATH:=${IMG_DIR}/${IMG_NAME}}"
-: "${IMG_URL:=https://cloud.debian.org/images/cloud/trixie/latest/debian-13-generic-amd64.qcow2}"
+: "${PASSWD:=ubuntu}"
+: "${IMG_DIR:=/var/pve/vm/template}"
+: "${IMG_EXT:=img}"
+: "${NEW_EXT:=qcow2}"
+: "${IMG_NAME:=noble-server-cloudimg-amd64}"
+: "${IMG_PATH:=${IMG_DIR}/${IMG_NAME}.${IMG_EXT}}"
+: "${NEW_PATH:=${IMG_DIR}/ubuntu-template.${NEW_EXT}}"
+: "${IMG_URL:=https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img}"
 
 echo "===> Installing dependencies"
 apt update
 apt install libguestfs-tools dhcpcd-base wget -y
 
+
+echo "===> Removing previous image"
+rm -f "${NEW_PATH}"
+
 echo "===> Downloading fresh ${IMG_NAME} image to ${IMG_DIR}"
 mkdir -p "${IMG_DIR}"
-rm -f "${IMG_PATH}"
 wget -O "${IMG_PATH}" "${IMG_URL}"
 
 echo "===> Customizing image located at ${IMG_PATH}"
-LIBGUESTFS_BACKEND=direct DEBIAN_FRONTEND=noninteractive virt-customize -v -x -a "${IMG_PATH}" \
-  --run-command "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config" \
-  --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
-  --run-command 'echo "nameserver 8.8.8.8" > /etc/resolv.conf' \
-  --run-command 'ping -c 3 8.8.8.8' \
-  --run-command 'ping -c 3 google.com' \
-  --install qemu-guest-agent,cloud-init \
-  --run-command 'systemctl enable qemu-guest-agent' \
-  --root-password "password:${PASSWD}"
 
-echo "===> DONE: Prepared image is ready at ${IMG_PATH}"
+virt-customize -a "${IMG_DIR}/${IMG_NAME}.${IMG_EXT}" --update --install qemu-guest-agent --install cloud-init \
+ --root-password password:"${PASSWD}" \
+ --run-command "echo -n > /etc/machine-id" \
+ --run-command "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config" \
+ --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
+ --run-command 'systemctl enable qemu-guest-agent'
+
+echo "===> Changing file image extension from ${IMG_EXT} to ${NEW_EXT}"
+mv "${IMG_PATH}" "${NEW_PATH}"
+
+echo "===> DONE: Prepared image is ready at ${NEW_PATH}"
